a3e709f8842011e163b4bef2da92093324bf6b92
d1vegg_PC <danil.plokhikh@gmail.com>
2026-03-14 01:51:33 +0300

    Update gitignore. show map func, create map func, added 1d vector for map, added setter func.

--- test.cpp
+++ test.cpp
@@ -1,19 +1,39 @@
 #include <iostream>
+#include <vector>
 
-int map(int width, int height) {
-	for (int i=0; i < height; i++)
-		std::cout << std::string(width, '.') << std::endl;
-	return 0;
+std::vector<char> create_map(int width, int height) {
+	std::vector<char> map(width*height);
+	for (int i=0; i < width*height; i++)
+		map[i] = '.';
+	return map;
 }
 
+void show_map(std::vector<char>& map, int width, int height) {
+	int y=0;
+	while (y<height) {
+		for (int x=0; x<width; x++)
+			std::cout << map[y*width+x];
+		std::cout << '\n';
+		y++;
+	}
+}
+
+std::vector<char> setCell(std::vector<char>& map, int x, int y, char value, int width)
+{
+	int index = y*width+x;
+	map[index] = value;
+	return map;
+}
 
 int main() {
-	map(20, 10);
+	int width = 20;
+	int height = 10;
+	std::vector<char> map {create_map(width, height)};
+	setCell(map, 10, 5, '#', width);
+	setCell(map, width-1, 0, '#', width);
+	setCell(map, 0, height-1, '#', width);
+	setCell(map, width-1, height-1, '#', width);
+	setCell(map, 0, 0, '#', width);
+	show_map(map, width, height);
 	return 0;
 }
-
-#TODO
-#
-#
-#
-#
